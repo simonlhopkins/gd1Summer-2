d552c0ce06f34b4506704e17be86024fd19da760
CARL\shoem <amyschumacher103@gmail.com>
2018-06-12 12:18:48 -0400

    Added pot/pan placeholder, script that spawns pots&pans and launches them in an arc

---
+++ Assets/Scripts/PotPanLaunch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotPanLaunch : MonoBehaviour {
+
+    public float thrust;
+    public float spinSpeed = 455.0f;      //rotation so that the pot slowly spins
+    public float launchAngle = 45.0f;     //angle of launch
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        transform.eulerAngles = new Vector3(launchAngle, launchAngle, 0);
+        rb = GetComponent<Rigidbody>();
+        Launch();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //I don't know why this works but this keeps the pots/pans from instantly rotating on play
+        spinSpeed += Input.GetAxis("Vertical");
+        transform.eulerAngles = new Vector3(0, spinSpeed, 0);
+    }
+
+    void Launch()
+    {
+        rb.AddForce(transform.eulerAngles * thrust);
+    }
+}
